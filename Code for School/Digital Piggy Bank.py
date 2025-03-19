6ac26f2b390b334e670f5d13274a798d063a368b
Andrew <122998824+Happy7213575574@users.noreply.github.com>
2025-03-19 09:22:17 +0000

    Added file handling

--- Code for School/Digital Piggy Bank.py
+++ Code for School/Digital Piggy Bank.py
@@ -1,13 +1,17 @@
 Coins =[['1p', 0.01], ['2p', 0.02], ['5p', 0.05], ['10p', 0.10], ['20p', 0.20], ['50p', 0.50], ['£1', 1.00], ['£2', 2.00]]
 total = 0
+file = open("Digital Piggy Bank.txt", "w")
 
 valid = False
 
 while valid == False:
     coinInput = input("Enter a coin: ")
     
-    if coinInput == "exit":
+    
+    if coinInput == "X":
         print(f"You have £{total}.")
+        file.write(str(total))
+        file.close()
         valid = True
     
     for i in range(len(Coins)):
