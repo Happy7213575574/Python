a3045e0f0e142eefda9f622bc879007e17205085
Andrew <122998824+Happy7213575574@users.noreply.github.com>
2025-03-19 09:18:29 +0000

    Made basic code with validation

--- Code for School/Digital Piggy Bank.py
+++ Code for School/Digital Piggy Bank.py
@@ -1,3 +1,18 @@
 Coins =[['1p', 0.01], ['2p', 0.02], ['5p', 0.05], ['10p', 0.10], ['20p', 0.20], ['50p', 0.50], ['£1', 1.00], ['£2', 2.00]]
 total = 0
 
+valid = False
+
+while valid == False:
+    coinInput = input("Enter a coin: ")
+    
+    if coinInput == "exit":
+        print(f"You have £{total}.")
+        valid = True
+    
+    for i in range(len(Coins)):
+        if coinInput == Coins[i][0]:
+            total += Coins[i][1]
+    
+    print(f"You have £{total}.")
+        
