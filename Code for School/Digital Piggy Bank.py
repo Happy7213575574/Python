7cbab0a396b37cedd5e1eea88e040b3f3e5af79c
Andrew <122998824+Happy7213575574@users.noreply.github.com>
2025-03-19 09:31:29 +0000

    Added file more file handling and

--- Code for School/Digital Piggy Bank.py
+++ Code for School/Digital Piggy Bank.py
@@ -1,21 +1,29 @@
+#get previous ammount from file
+file = open("dpb.txt", "r")
+
+for line in file:
+    data = file.readline()
+file.close()
+total = float(data)
+print(f"Your previous total was £{total}.")
+
 #declare variables
 Coins =[['1p', 0.01], ['2p', 0.02], ['5p', 0.05], ['10p', 0.10], ['20p', 0.20], ['50p', 0.50], ['£1', 1.00], ['£2', 2.00]]
-total = 0
 valid = False
-
-#open file to be written to
-file = open("Digital Piggy Bank.txt", "w")
 
 #run while loop until user enters X
 while valid == False:
-    coinInput = input("Enter a coin: ")
+    coinInput = input("Enter a coin (enter X to end program): ").lower()
     
     #check input
-    if coinInput == "X":
+    if coinInput == "x":
         print(f"You have £{total}.")
+        #open file to be written to and close file
+        file = open("Coins.txt", "w")
         file.write(str(total))
         file.close()
         valid = True
+        #end while
     
     else:
         #caculate coin value
@@ -28,6 +36,5 @@ while valid == False:
     
     #print total
     print(f"You have £{total}.")
-#end while
 #end program
         
