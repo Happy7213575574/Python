3953e3ecdbd5a78f0a5f5b9a2f0e34fdbaccdca4
Andrew <122998824+Happy7213575574@users.noreply.github.com>
2025-03-28 13:13:06 +0000

    Did Fundamentals 3

--- Code for School/11ccs-Programing-Practice-Refresher-2025/Fundamentals #3/main.py
+++ Code for School/11ccs-Programing-Practice-Refresher-2025/Fundamentals #3/main.py
@@ -10,13 +10,28 @@ https://www.w3schools.com/python/python_strings_slicing.asp
 
 '''
 
+string = "abcdefghijklmnopqrstuvwxyz"
 
 # write Python to extract and print the left 5 characters of the string above
 
+print(string[0:5])
+
 # do the same to get the middle 5 characters
+
+print(string[10:15])
 
 # now do the same to get the last 4 characters
 
+print(string[-4:])
+
 # what is the command to find the length of a string ?
 
+length = len(string)
+print(length)
+
 # what is the command to reverse a string in Python ?
+
+ReverseString = string[::-1]
+print(ReverseString)
+
+#end program
