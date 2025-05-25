76782fac301db06ca87c9e594bfa48312ad660f0
Andrew <122998824+Happy7213575574@users.noreply.github.com>
2025-05-25 13:54:36 +0100

    xhnaged HnagmanTurtle

--- MostCode/HangmanTurtle.py
+++ MostCode/HangmanTurtle.py
@@ -6,31 +6,13 @@ from turtle import Turtle as Tu
 screen = tu.Screen()
 screen.setup(1.0, 1.0)
 
-#Make the person for turtle
-person_shape = [
-    (0, -20),    # bottom
-    (0, 20),     # body up
-    (-10, 40),   # left arm
-    (0, 20),     # back to body
-    (10, 40),    # right arm
-    (0, 20),     # back to body
-    (0, 60),     # neck
-    (-10, 70),   # left side of head
-    (-10, 80),   # top left
-    (10, 80),    # top right
-    (10, 70),    # right side
-    (0, 60)      # close head
-]
-
-# Register and use the shape
-tu.register_shape("stick_person", person_shape)
+screen.addshape("person.gif")  # Add your image file here
 
 #make the turtle
 t = Tu()
-t.shape("stick_person")
+t.shape("person.gif")  # Set the turtle to use the image
 tu.done()
 t.speed(50) # set it's speed
-
 t.setposition(0,0) #set the turtle
 
 t.pensize(15) # make the pen thicker
