9b24e6da47d46c363154f96683a5211161f3382e
Andrew <122998824+Happy7213575574@users.noreply.github.com>
2025-05-25 12:39:25 +0100

    Made turtle as a stick figure in HangmanTurtle

--- MostCode/HangmanTurtle.py
+++ MostCode/HangmanTurtle.py
@@ -5,7 +5,29 @@ import turtle as tu
 screen = tu.Screen()
 screen.setup(1.0, 1.0)
 
+import turtle
+
+#Make the person
+person_shape = [
+    (0, -20),    # bottom
+    (0, 20),     # body up
+    (-10, 40),   # left arm
+    (0, 20),     # back to body
+    (10, 40),    # right arm
+    (0, 20),     # back to body
+    (0, 60),     # neck
+    (-10, 70),   # left side of head
+    (-10, 80),   # top left
+    (10, 80),    # top right
+    (10, 70),    # right side
+    (0, 60)      # close head
+]
+
+# Register and use the shape
+turtle.register_shape("stick_person", person_shape)
+
 #make the turtle
 t = tu.Turtle()
-t.shape('turtle') # set it's shape
+t.shape("stick_person")
+turtle.done()
 t.speed(50) # set it's speed
