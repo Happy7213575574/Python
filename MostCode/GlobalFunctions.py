07493a04b19cd13a9e591f9b88ad675099584446
Andrew <122998824+Happy7213575574@users.noreply.github.com>
2025-06-07 11:11:26 +0100

    delted unneded files and moved a function

--- MostCode/GlobalFunctions.py
+++ MostCode/GlobalFunctions.py
@@ -1,4 +1,4 @@
-global stringInput, numberInput
+global stringInput, numberInput, CollatzSequence
 
 def stringInput(reason):
   valid = False
@@ -22,6 +22,40 @@ def numberInput():
       except ValueError:
         print('That is not a number.') 
   return Input
+#end def
+
+def CollatzSequence():
+  # import  numberInput function
+  from GlobalFunctions import  numberInput
+
+  end = False
+  
+  while end != True:
+    # run function  numberInput
+    NumberInput = numberInput()
+
+    # count the steps
+    steps = 0
+
+    # do the squence
+    for i in range(9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999):
+      if  NumberInput == 1 or NumberInput == 0:
+        break
+      elif  NumberInput % 2 == 0:
+          NumberInput =  NumberInput / 2
+          steps = steps + 1
+      else:
+          NumberInput = (3 * NumberInput) + 1
+          steps = steps + 1
+      # end if
+    #end for
+              
+    if  NumberInput == 1:
+        print("It took", steps, "steps")
+    else:
+        print("The  number didn't reach 1 yet")
+    #end if
+    return steps
 #end def
 
 global grpahic, CurrentDriversList, HistoricDriversList, CurrentTeamsList, HistoricalTeamsList, Wordsdictionary
