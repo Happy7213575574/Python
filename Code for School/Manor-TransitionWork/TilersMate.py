fa3efbc275cbc6f1e3aa80ee330b65bccb457444
Andrew <122998824+Happy7213575574@users.noreply.github.com>
2025-06-30 19:38:43 +0000

    made while loop to validate input and added instructions

---
+++ Code for School/Manor-TransitionWork/TilersMate.py
@@ -0,0 +1,26 @@
+"""
+Create a program that:  
+o Asks the user to enter the width and length of the floor to be tiled  
+o Enter the cost per square metre of the tiles to be used  
+o Calculate the cost of the tiles to be purchased  
+o Calculate the length of time it would take a tiler to complete the task (1 hour for 
+every 2 metres squared of tiling)  
+o Calculate the cost of the entire job (tiles + labour) and display this without VAT 
+and with VAT (an additional 20%) 
+"""
+#decalare variables
+VAT = 1.20 # set VAT rate
+valid = False # to control "while" loops
+
+# create a while loop to get the length
+while valid != True: # use a loop to validate user input
+    try: # check that the input is a number
+        length = int(input("What is the length of the floor to be tiled?: ")) # get the length of the user
+        if str(length) != "": # perform a presence check
+            valid = True # end the loop
+        elif str(length) == "": 
+            print('You must enter a number') # tell the user that it can not be blank
+        else: # last resort for errors
+            print("An error has occured.") # tell the user something has gone wrong
+    except ValueError: # check the input can be a number
+        print("Please enter a number.") # tell the user that a number must be inputed
