874f7b55cf2af412c82b7ea704a6c5e9bbcdcfa1
Andrew <122998824+Happy7213575574@users.noreply.github.com>
2025-07-01 12:42:05 +0000

    Added instructions to PasswordGenerator

---
+++ Code for School/Manor-TransitionWork/PasswordGenerator.py
@@ -0,0 +1,12 @@
+"""
+Random password generator. Create a program that:  
+o Asks the user how many characters should be in the password  
+o Asks the user how complex the password should be; weak, medium or strong  
+o Creates a random password of the appropriate length to be the correct password 
+strength 
+▪ Weak passwords should be all letters  
+▪ Medium passwords should include at least one number  
+▪ Strong passwords should include at least one number and one special 
+character ( + - / & ! ) 
+o Display the suggested password 
+"""
