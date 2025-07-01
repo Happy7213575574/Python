9ed39b27abb800802be8a0d4206a2ed009065106
Andrew <122998824+Happy7213575574@users.noreply.github.com>
2025-07-01 12:48:42 +0000

    Added while loop to get the strength of password. Done in PasswordGenerator

--- Code for School/Manor-TransitionWork/PasswordGenerator.py
+++ Code for School/Manor-TransitionWork/PasswordGenerator.py
@@ -4,9 +4,9 @@ o Asks the user how many characters should be in the password
 o Asks the user how complex the password should be; weak, medium or strong  
 o Creates a random password of the appropriate length to be the correct password 
 strength 
-▪ Weak passwords should be all letters  
-▪ Medium passwords should include at least one number  
-▪ Strong passwords should include at least one number and one special 
+    ▪ Weak passwords should be all letters  
+    ▪ Medium passwords should include at least one number  
+    ▪ Strong passwords should include at least one number and one special 
 character ( + - / & ! ) 
 o Display the suggested password 
 """
@@ -24,4 +24,13 @@ while valid != True:
     except ValueError:
         print("Please enter a number")
     # end try
+# end while
+
+while valid != False:
+    strength = input("How strong would you like the password (weak, medium or strong)?:").lower()
+    if strength == "weak" or strength == "medium" or strength == "strong":
+        valid = False
+    elif strength != "weak" or strength != "medium" or strength != "strong":
+        print("Please pick how strong you would like the password, weak, medium or strong.")
+    # end if
 # end while
