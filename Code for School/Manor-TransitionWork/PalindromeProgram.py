6886c7e1e50583608f9086b90a0827979a5175c0
Andrew <122998824+Happy7213575574@users.noreply.github.com>
2025-07-01 12:29:23 +0000

    Added instructions to  Palindrome program.

---
+++ Code for School/Manor-TransitionWork/PalindromeProgram.py
@@ -0,0 +1,9 @@
+"""
+Palindrome program. Create a program that: 
+o The program asks for a word to be entered by the user  
+o Tells the user how many characters are in the word  
+o Displays the word in reverse  
+o Tells the user whether the word is a palindrome or not (it can be spelled the same 
+backwards and forward) 
+"""
+
