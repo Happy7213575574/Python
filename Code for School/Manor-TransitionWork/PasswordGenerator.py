91f271f81d28ce7cfdc79537290af3ace3c1f1f0
Andrew <122998824+Happy7213575574@users.noreply.github.com>
2025-07-01 12:54:39 +0000

    Added lists of letters to PasswordGenerator

--- Code for School/Manor-TransitionWork/PasswordGenerator.py
+++ Code for School/Manor-TransitionWork/PasswordGenerator.py
@@ -10,8 +10,14 @@ strength
 character ( + - / & ! ) 
 o Display the suggested password 
 """
+from random import randint as r
 
 valid = False
+
+letters = [
+["q","w","e","r","t",'y','u','i','o','p','a','s','d','f','g','h','j','k','l','z''x','c','v','b','n','m'],
+['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']
+]
 
 while valid != True:
     try:
@@ -34,3 +40,12 @@ while valid != False:
         print("Please pick how strong you would like the password, weak, medium or strong.")
     # end if
 # end while
+
+if strength == "weak":
+    
+elif strength == "medium":
+    
+elif strength == "strong":
+    
+else:
+    print("You did not pick one of the options or an error has occured")
