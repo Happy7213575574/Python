1ae42a757b2a1112e11f9cc501a6f3348b2d9712
Andrew <122998824+Happy7213575574@users.noreply.github.com>
2025-07-10 09:50:52 +0100

    Update GCSE TO A-LEVEL (HBK).py
    reformatted the comment in HBK transition work

--- Code for School/GCSE TO A-LEVEL (HBK).py
+++ Code for School/GCSE TO A-LEVEL (HBK).py
@@ -6,6 +6,7 @@ Design a solution that provides a means to validate a 16 digit Credit Card numbe
 solution must validate a Credit Card number that has been entered by the user through a simple Graphical User 
 Interface and provide specific responses as to what is wrong with the Credit Card number. 
 The Luhn’s algorithm should be used.
+
 Extension
 Provide a feature to validate a set of Credit Card numbers that are held in a Comma Separated Value (CSV) file.
 """
