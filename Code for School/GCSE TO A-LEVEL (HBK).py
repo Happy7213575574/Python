718e946e017070789c74fbfcf1b71c7421adedb2
Andrew <122998824+Happy7213575574@users.noreply.github.com>
2025-07-10 09:46:41 +0100

    Create GCSE TO A-LEVEL (HBK).py
    add

---
+++ Code for School/GCSE TO A-LEVEL (HBK).py
@@ -0,0 +1,11 @@
+"""
+Project Brief
+Students must research, analyze, design, code and test how to provide a solution to the following problem.
+Document all assumptions and assertions.
+Design a solution that provides a means to validate a 16 digit Credit Card number from Visa or Mastercard only. The 
+solution must validate a Credit Card number that has been entered by the user through a simple Graphical User 
+Interface and provide specific responses as to what is wrong with the Credit Card number. 
+The Luhn’s algorithm should be used.
+Extension
+Provide a feature to validate a set of Credit Card numbers that are held in a Comma Separated Value (CSV) file.
+"""
